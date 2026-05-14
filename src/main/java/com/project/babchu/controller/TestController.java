d8453cbc1533ed98dc51799573b885e9f284fa03
EugeneTA <0xdf0101@gmail.com>
2026-05-14 10:55:03 +0900

    config: docker-compose, nginx.conf 작성

--- src/main/java/com/project/babchu/controller/TestController.java
+++ src/main/java/com/project/babchu/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
 
     @GetMapping("/")
-    public void test() {
+    public String test() {
         return "test";
     }
 }
