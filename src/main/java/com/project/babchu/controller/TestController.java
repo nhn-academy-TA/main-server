132a0566c45f5771337b71a39f2bc24a22d2262d
EugeneTA <0xdf0101@gmail.com>
2026-05-14 09:43:10 +0900

    feat: main page

---
+++ src/main/java/com/project/babchu/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.project.babchu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public void test() {
+        return "test";
+    }
+}
