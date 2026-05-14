ef06c04adb53f98ff85c0a006ef602a3124ca610
EugeneTA <0xdf0101@gmail.com>
2026-05-14 15:09:19 +0900

    test: PR test

--- src/main/java/com/project/babchu/controller/TestController.java
+++ src/main/java/com/project/babchu/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.project.babchu.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class TestController {
 
     @GetMapping("/")
     public String test() {
+        log.info("CI/CD 테스트");
         return "test";
     }
 }
