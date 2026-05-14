a880e974f396f9883fbb364d5d7ec3add5399a43
EugeneTA <0xdf0101@gmail.com>
2026-05-14 09:39:26 +0900

    init

---
+++ src/main/java/com/project/babchu/BabchuApplication.java
@@ -0,0 +1,13 @@
+package com.project.babchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabchuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabchuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/babchu/BabchuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.babchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabchuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
